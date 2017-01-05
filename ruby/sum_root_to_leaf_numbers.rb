710a65969aaff55a7da85d34652cc43f54f83eea
Villins <linshao512@gmail.com>
2017-01-05 12:32:42 +0800

    use number instead of string cancat

--- ruby/sum_root_to_leaf_numbers.rb
+++ ruby/sum_root_to_leaf_numbers.rb
@@ -3,18 +3,18 @@ def sum_numbers(root)
   sum = 0
   return sum unless root
   queue = []
-  queue.push([root, root.val.to_s])
+  queue.push([root, root.val])
   while(queue.any?) do
     node, val = queue.pop
     if node.left || node.right
       if node.left
-        queue.push([node.left, val+node.left.val.to_s])
+        queue.push([node.left, val * 10 + node.left.val])
       end
       if node.right
-        queue.push([node.right, val+node.right.val.to_s])
+        queue.push([node.right, val * 10 + node.right.val])
       end
     else
-      sum = sum + val.to_i
+      sum = sum + val
     end
   end
   return sum
