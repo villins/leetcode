97c8d9a8cffd1e17b457ea55aa4866454ba67fa9
Villins <linshao512@gmail.com>
2017-06-03 14:57:17 +0800

    accepted 560. Subarray Sum Equals K with map

--- golang/sub_array.go
+++ golang/sub_array.go
@@ -17,7 +17,29 @@ func subarraySum(nums []int, k int) int {
 	return result
 }
 
+func subarraySum2(nums []int, k int) int {
+	result := 0
+	sum := 0
+	l := len(nums)
+	nMap := make(map[int]int)
+	nMap[0] = 1
+	for i := 0; i < l; i++ {
+		sum += nums[i]
+		if v, ok := nMap[sum-k]; ok {
+			result += v
+		}
+
+		if v, ok := nMap[sum]; ok {
+			nMap[sum] = v + 1
+		} else {
+			nMap[sum] = 1
+		}
+	}
+	return result
+}
+
 func main() {
 	array := []int{1, 1, 1}
 	fmt.Println(subarraySum(array, 2))
+	fmt.Println(subarraySum2(array, 2))
 }
